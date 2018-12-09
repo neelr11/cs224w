362180825398778227ba1b11e48d877e27721eaa
harper <hcarroll@stanford.edu>
2018-12-09 00:07:34 -0800

    node2vec songs

--- src/generate_songs_v2.py
+++ src/generate_songs_v2.py
@@ -1,10 +1,11 @@
-
 import load_genre_graphs as ld
 import analysis
 import snap
 import numpy as np
 import random
+import networkx as nx
 from collections import defaultdict
+import node2vec as nv
 
 ITERATIONS = 100
 OUT_THRESHOLD = 0.7
@@ -15,9 +16,9 @@ def random_walk_generation(G_Multi, i, id_to_chord, genre):
     chord_progression = []
     length = 0
     #instantiate song graph
-    G_Directed_i = snap.TNGraph.New() #song graph i
+    #G_Directed_i = snap.TNGraph.New() #song graph i
     node = G_Multi.BegNI()
-    G_Directed_i.AddNode(node.GetId())
+    #G_Directed_i.AddNode(node.GetId())
 
     #while not at last node in the song
     while node.GetOutDeg() > 0 and length < MAX_SONG_LENGTH:
@@ -25,20 +26,19 @@ def random_walk_generation(G_Multi, i, id_to_chord, genre):
         chord_progression.append(id_to_chord[nodeID])
         neighbors = [node.GetNbrNId(x) for x in xrange(node.GetOutDeg())]
         dstID = random.choice(neighbors)
-        if not G_Directed_i.IsNode(dstID): #if edge dst isn't a node in G_Directed_i, add it
-            G_Directed_i.AddNode(dstID)
-        G_Directed_i.AddEdge(nodeID, dstID)
+    #    if not G_Directed_i.IsNode(dstID): #if edge dst isn't a node in G_Directed_i, add it
+    #        G_Directed_i.AddNode(dstID)
+    #    G_Directed_i.AddEdge(nodeID, dstID)
         node = G_Multi.GetNI(dstID)
         length += 1
 
     #add last chord to list
     chord_progression.append(id_to_chord[nodeID])
 
-    file_name = "generated_songs/random_generated_" + genre + "_song_" + str(i+1) + ".txt"
-    snap.SaveEdgeList(G_Directed_i, file_name)
+    #file_name = "generated_songs/random_generated_" + genre + "_song_" + str(i+1) + ".txt"
+    #snap.SaveEdgeList(G_Directed_i, file_name)
 
-    fname = "Random_generated_" + genre + "_songs.txt"
-    return fname, chord_progression
+    return chord_progression
 
 
 def smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i = ""):
@@ -99,37 +99,69 @@ def smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i = ""):
     #add last chord to list
     chord_progression.append(id_to_chord[nodeID])
 
-    if i is not "":
-        file_name = "generated_songs/smart_generated_" + genre + "_song_" + str(i+1) + ".txt"
-    else:
-        file_name =  "generated_songs/smart_generated_" + genre + "_song.txt"
+    #if i is not "":
+#        file_name = "generated_songs/smart_generated_" + genre + "_song_" + str(i+1) + ".txt"
+#    else:
+#        file_name =  "generated_songs/smart_generated_" + genre + "_song.txt"
 
     #Save generated graph
     #snap.SaveEdgeList(G_Directed_i, file_name)
 
-    fname = "Smart_generated_" + genre + "_songs.txt"
-    return fname, chord_progression
+    return chord_progression
 
 
-def main(Random=False):
+def node2vec(G_Multi, G_nx, id_to_chord, genre, i):
+
+    #if args.weighted:
+    #	G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.MultiGraph())
+    #else:
+
+    #if not args.directed:
+    #	G = G.to_undirected()
+    is_directed = True
+    id_to_outdegree = {node.GetId() : node.GetOutDeg() for node in G_Multi.Nodes()}
+    weighted_list = []
+    for id in id_to_outdegree:
+        weighted_list.extend([id] * id_to_outdegree[id])
+    startID = random.choice(weighted_list)
+
+    p, q = 5, 100 #want to have high prob of going back, and low p of going forward
+    G = nv.Graph(G_nx, is_directed, p, q)
+    G.preprocess_transition_probs()
+    walk = G.node2vec_walk(MAX_SONG_LENGTH, startID)
+
+    chord_progression = []
+    for nodeID in walk:
+        chord_progression.append(id_to_chord[nodeID])
+    return chord_progression
+
+
+def main(type):
     for genre in ["rock", "jazz"]:
         #Instantiate datasets
-        G_Multi, G_Directed, G_Undirected, id_to_chord = ld.load_genre_graphs(genre)
+        G_Multi, G_Directed, G_Undirected, G_nx, id_to_chord = ld.load_genre_graphs(genre)
         generated_songs = [] #list of song graphs
 
-        if (Random == True):
+        if (type == "random"):
             for i in range(ITERATIONS):
-                fname, chord_progression = random_walk_generation(G_Multi, i, id_to_chord, genre)
+                chord_progression = random_walk_generation(G_Multi, i, id_to_chord, genre)
                 while len(chord_progression) < MAX_SONG_LENGTH * 0.8:
-                    fname, chord_progression = random_walk_generation(G_Multi, i, id_to_chord, genre)
+                    chord_progression = random_walk_generation(G_Multi, i, id_to_chord, genre)
                 generated_songs.append(chord_progression)
-        else:
+        elif (type == "smart"):
             for i in range(ITERATIONS):
-                fname, chord_progression = smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i)
+                chord_progression = smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i)
                 while len(chord_progression) < MAX_SONG_LENGTH * 0.8:
-                    fname, chord_progression = smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i)
+                    chord_progression = smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i)
+                generated_songs.append(chord_progression)
+        elif (type == "node2vec"):
+            for i in range(ITERATIONS):
+                chord_progression = node2vec(G_Multi, G_nx, id_to_chord, genre, i)
+                while len(chord_progression) < MAX_SONG_LENGTH * 0.8:
+                    chord_progression = node2vec(G_Multi, G_nx, id_to_chord, genre, i)
                 generated_songs.append(chord_progression)
 
+        fname = type + "_generated_" + genre + "_songs.txt"
         #Save generated song
         f = open(fname, "w")
         index = 1
@@ -141,4 +173,5 @@ def main(Random=False):
             index += 1
 
 if __name__ == "__main__":
-    main()
+    for type in ["node2vec"]:
+        main(type)
--- src/load_genre_graphs.py
+++ src/load_genre_graphs.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import snap
+import networkx as nx
 
 with open('chords_dict_jazz.txt', 'r') as file:
      dict = (json.load(file))
@@ -13,13 +14,22 @@ with open('chords_dict_rock.txt', 'r') as file:
 
 def load_genre_graphs(graph):
     if graph == "rock":
-        filename = '../data/genre_graphs/rock_graph/rock_graph.txt'
+        filename = '../data/genre_graphs/rock_graph/rock_graph.edgelist'
         dict = chords_dict_rock
     else:
-        filename = '../data/genre_graphs/jazz_graph/jazz_graph.txt'
+        filename = '../data/genre_graphs/jazz_graph/jazz_graph.edgelist'
         dict = chords_dict_jazz
 
     G_Multi = snap.LoadEdgeList(snap.PNEANet, filename, 0, 1)
     G_Directed = snap.LoadEdgeList(snap.PNGraph, filename, 0, 1)
     G_Undirected = snap.LoadEdgeList(snap.PUNGraph, filename, 0, 1)
-    return G_Multi, G_Directed, G_Undirected, dict
+
+
+    #hacky way to get by the mutligraph issue
+    G_nx_multi = nx.read_edgelist(filename, nodetype=int, create_using=nx.MultiGraph())
+    G_nx = nx.read_edgelist(filename, nodetype=int, create_using=nx.DiGraph())
+
+    for edge in G_nx.edges():
+        G_nx[edge[0]][edge[1]]['weight'] = G_nx_multi.number_of_edges(edge[0], edge[1])
+
+    return G_Multi, G_Directed, G_Undirected, G_nx, dict
