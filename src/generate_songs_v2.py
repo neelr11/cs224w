5fe8c785aee5e7cb639e91ea0478cad7547fe837
harper <hcarroll@stanford.edu>
2018-12-09 23:22:48 -0800

    added graph export for smart andrt and biased

--- src/generate_songs_v2.py
+++ src/generate_songs_v2.py
@@ -16,9 +16,9 @@ def random_walk_generation(G_Multi, i, id_to_chord, genre):
     chord_progression = []
     length = 0
     #instantiate song graph
-    #G_Directed_i = snap.TNGraph.New() #song graph i
+    G_Directed_i = snap.TNGraph.New() #song graph i
     node = G_Multi.BegNI()
-    #G_Directed_i.AddNode(node.GetId())
+    G_Directed_i.AddNode(node.GetId())
 
     #while not at last node in the song
     while node.GetOutDeg() > 0 and length < MAX_SONG_LENGTH:
@@ -26,17 +26,17 @@ def random_walk_generation(G_Multi, i, id_to_chord, genre):
         chord_progression.append(id_to_chord[nodeID])
         neighbors = [node.GetNbrNId(x) for x in xrange(node.GetOutDeg())]
         dstID = random.choice(neighbors)
-    #    if not G_Directed_i.IsNode(dstID): #if edge dst isn't a node in G_Directed_i, add it
-    #        G_Directed_i.AddNode(dstID)
-    #    G_Directed_i.AddEdge(nodeID, dstID)
+        if not G_Directed_i.IsNode(dstID): #if edge dst isn't a node in G_Directed_i, add it
+            G_Directed_i.AddNode(dstID)
+        G_Directed_i.AddEdge(nodeID, dstID)
         node = G_Multi.GetNI(dstID)
         length += 1
 
     #add last chord to list
     chord_progression.append(id_to_chord[nodeID])
 
-    #file_name = "generated_songs/random_generated_" + genre + "_song_" + str(i+1) + ".txt"
-    #snap.SaveEdgeList(G_Directed_i, file_name)
+    file_name = "generated_songs/random_generated_" + genre + "_song_" + str(i+1) + ".txt"
+    snap.SaveEdgeList(G_Directed_i, file_name)
 
     return chord_progression
 
@@ -48,7 +48,7 @@ def smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i = ""):
     chord_progression = []
 
     #instantiate song graph
-    #G_Directed_i = snap.TNGraph.New() #song graph i
+    G_Multi_i = snap.TNEANet.New() #song graph i
 
     #Get node in largest cluster
     #1. Get index of largest element of clusters
@@ -57,7 +57,7 @@ def smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i = ""):
     possible_next = clusters[cluster_index]
     nodeID = random.choice(clusters[cluster_index])
 
-    #G_Directed_i.AddNode(nodeID)
+    G_Multi_i.AddNode(nodeID)
     node = G_Multi.GetNI(nodeID)
 
     #while not at last node in the song
@@ -87,10 +87,10 @@ def smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i = ""):
             cluster_index = [i for i, x in enumerate(clusters) if dstID in x][0]
 
         #if edge dst isn't a node in G_Directed_i, add it
-        #if not G_Directed_i.IsNode(dstID):
-        #    G_Directed_i.AddNode(dstID)
+        if not G_Multi_i.IsNode(dstID):
+            G_Multi_i.AddNode(dstID)
 
-    #    G_Directed_i.AddEdge(nodeID, dstID)
+        G_Multi_i.AddEdge(nodeID, dstID)
         node = G_Multi.GetNI(dstID)
         #Update node to current node
         nodeID = dstID
@@ -99,13 +99,13 @@ def smart_walk_generation(G_Undirected, G_Multi, id_to_chord, genre, i = ""):
     #add last chord to list
     chord_progression.append(id_to_chord[nodeID])
 
-    #if i is not "":
-#        file_name = "generated_songs/smart_generated_" + genre + "_song_" + str(i+1) + ".txt"
-#    else:
-#        file_name =  "generated_songs/smart_generated_" + genre + "_song.txt"
+    if i is not "":
+        file_name = "generated_songs/smart_generated_" + genre + "_song_" + str(i+1) + ".txt"
+    else:
+        file_name =  "generated_songs/smart_generated_" + genre + "_song.txt"
 
     #Save generated graph
-    #snap.SaveEdgeList(G_Directed_i, file_name)
+    snap.SaveEdgeList(G_Multi_i, file_name)
 
     return chord_progression
 
@@ -131,8 +131,23 @@ def node2vec(G_Multi, G_nx, id_to_chord, genre, i):
     walk = G.node2vec_walk(MAX_SONG_LENGTH, startID)
 
     chord_progression = []
-    for nodeID in walk:
+
+
+    if i is not "":
+        file_name = "generated_songs/smart_generated_" + genre + "_song_" + str(i+1) + ".txt"
+    else:
+        file_name =  "generated_songs/smart_generated_" + genre + "_song.txt"
+
+    file = open(file_name, "w")
+    for index, nodeID in enumerate(walk):
         chord_progression.append(id_to_chord[nodeID])
+        if index is not 0:
+            file.write(id_to_chord[nodeID] + "\n")
+        if index is not len(walk) - 1:
+            file.write(id_to_chord[nodeID] + " ",)
+
+    file.close()
+
     return chord_progression
 
 
